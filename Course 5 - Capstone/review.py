cac8e307022d86117181faa68d633e4a27c46b25
tylersng <tylersng@gmail.com>
2025-05-08 21:16:40 +0800

    Capstone Review

---
+++ Course 5 - Capstone/review.py
@@ -0,0 +1,20 @@
+#print(ord('G'))
+"""
+asclist = [108,105,115,116]
+for x in asclist:
+    print(chr(x))"""
+"""
+zap = "hello there bob"
+print(zap[4])
+"""
+
+"""
+total = 0
+for abc in range(5):
+    print(abc)
+    total = total + abc
+print(total)
+"""
+abc = "with three words" 
+stuff = abc.split()
+print(stuff)
